b96cf48a871f132d0253ab58940facc4bdcd641c
mrstevengz <ssequeira@uamv.edu.ni>
2026-05-01 22:04:06 -0600

    Se crea un objeto con un personaje aleatorio (para intentar adivinar) cada vez que se refresca la aplicacion o se le da click al boton de jugar de nuevo, se comparan los campos con este personaje y si es correcto se termina el juego, y se muestra el modal con un timeout de 1 segundo (para que carge la animacion)

--- client/src/components/CharacterResponse.jsx
+++ client/src/components/CharacterResponse.jsx
@@ -1,14 +1,23 @@
-function CharacterResponse({selectedCharacter}) {
-    if (!selectedCharacter) {
+function CharacterResponse({selectedCharacter, secretCharacter}) {
+    if (!selectedCharacter || !secretCharacter) {
         return null;
     }
 
     return (
         <div className="character-response">
-            <h2>{selectedCharacter.name}</h2>
-            <p>{selectedCharacter.series}</p>
-            <p>{selectedCharacter.gender}</p>
-            <p>{selectedCharacter.hair_color}</p>
+            <h2 className={`response-value ${secretCharacter.name === selectedCharacter.name ? "correct" : ""}`}>{selectedCharacter.name}</h2>
+
+            <p className={`response-value ${secretCharacter.series === selectedCharacter.series ? "correct" : ""}`}>
+            {selectedCharacter.series}
+            </p>
+
+            <p className={`response-value ${secretCharacter.gender === selectedCharacter.gender ? "correct" : ""}`}>
+            {selectedCharacter.gender}
+            </p>
+
+            <p className={`response-value ${secretCharacter.hair_color === selectedCharacter.hair_color ? "correct" : ""}`}>
+            {selectedCharacter.hair_color}
+            </p>
         </div>
     );
 }
--- client/src/components/InputCharacter.jsx
+++ client/src/components/InputCharacter.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CharacterCard from "./CharacterCard";
 import CharacterResponse from "./CharacterResponse";
 import ResetModal from "./ResetModal";
@@ -8,6 +8,7 @@ function InputCharacter({ characters }) {
     const [text, setText] = useState("");
     const [selectedCharacters, setSelectedCharacters] = useState([]);
     const characterList = Array.isArray(characters) ? characters : [];
+    
 
     //Variable para manejar el abrir/cerrar del modal cuando termina un juego
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -15,14 +16,28 @@ function InputCharacter({ characters }) {
     //Variables para manejar la cantidad de intentos/guesses
     const MAX_GUESSES = 5;
     const currentGuessNumber = Math.min(selectedCharacters.length + 1, MAX_GUESSES);
-    const isGameOver = selectedCharacters.length >= MAX_GUESSES;
+    const [hasWon, setHasWon] = useState(false)
+    const hasLost = selectedCharacters.length >= MAX_GUESSES;
+    const isGameOver = hasWon || hasLost
+
+    const response_animation_ms = 1000
+    
 
     //Variable para almacener los valores de los personajes en un array y quitarles espacios/mayusculas
     const filteredCharacters = characterList.filter((character) =>
         character.name.toLowerCase().includes(text.trim().toLowerCase())
     );
 
-  function handleSelectCharacter(character) {
+    //Variables para escoger un personaje aleatorio
+    const [secretCharacter, setSecretCharacter] = useState(null)
+
+
+    function delay(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms))
+    }
+
+    //Funcion para almacenar cada personaje que se clickea a un arreglo
+  async function handleSelectCharacter(character) {
     if (isGameOver) return;
 
     const nextCharacters = [...selectedCharacters, character];
@@ -30,11 +45,42 @@ function InputCharacter({ characters }) {
 
     setSelectedCharacters(nextCharacters);
     setText("");
+    await delay(response_animation_ms)
+    correctGuess(character)
+    
 
     if (nextCharacters.length === MAX_GUESSES) {
         setIsModalOpen(true);
+        }
     }
-}
+
+    function correctGuess(character) {
+        if(character._id === secretCharacter._id) {
+            setHasWon(true)
+            setIsModalOpen(true)
+        }
+    }
+
+    //Funcion para empezar un nuevo juego (resetear personajes, escoger un personaje aleatorio)
+
+    function startNewGame() {
+        if (characterList.length === 0) return
+
+        //Escoger un personaje aleatorio
+        const randIndex = Math.floor(Math.random() * characterList.length)
+        const randChar = characterList[randIndex]
+        //Guardar el personaje en un objeto
+        setSecretCharacter(randChar)
+        setText("")
+        setSelectedCharacters([])
+        setHasWon(false)
+    }
+
+
+    //Cada vez que se recarga, se empieza un juego nuevo y se crea un personaje aleatorio. Eventualmente se cambiara, ya que se debe almacenar las respuestas en local storage y conservarse aunque se recarge
+    useEffect(() => {
+        startNewGame()
+    }, [characters])
 
 
     function handleCloseModal() {
@@ -52,6 +98,8 @@ function InputCharacter({ characters }) {
                     placeholder={`Guess ${currentGuessNumber} of ${MAX_GUESSES}`}
                     disabled={isGameOver}
                 />
+
+                {/* Regresar la lista de personajes en la db, mientras el juego no haya terminado */}
                 {!isGameOver && text.trim() && (
                     <div className="input-dropdown">
                         {filteredCharacters.length > 0 ? 
@@ -69,24 +117,34 @@ function InputCharacter({ characters }) {
                     </div>
                 )}
 
+                {/* Categorias encima de las respuestas (hard coded) */}
+
                 {selectedCharacters.length > 0 && (
                     <div className="response-categories">
                         <h3>Name</h3>
                         <p>Series</p>
+                        <p>Gender</p>
                         <p>Hair Color</p>
-                        <p>Age</p>
                     </div>
                 )}
+
+                {/* Respuestas al elegir un personaje */}
 
                 {selectedCharacters.map((character, index) => {
                     return(
                     <CharacterResponse key = {`${character._id || character.name}-${index}`} 
-                    selectedCharacter={character} />
+                    selectedCharacter={character} secretCharacter={secretCharacter}/>
                     )
                 })}
 
+                {/* Boton para jugar de nuevo */}
+
+                {isGameOver && <button className="button-play" onClick={startNewGame}>Play Again</button>}
+
+                {/* Modal cuando se PIERDE el juego (5/5 intentos) */}
+
                 <ResetModal isOpen={isModalOpen} onClose = {handleCloseModal}>
-                    {isModalOpen == true && (
+                    {isModalOpen === true && (
                         <div>
                             <h2>YOU LOST</h2>
                         </div>
